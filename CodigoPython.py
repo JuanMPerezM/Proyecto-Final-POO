839f7fdf2774d627472dae358b93098933362d03
MateoDN <99050162+MateoDN@users.noreply.github.com>
2022-03-25 09:03:09 -0500

    Update CodigoPython.py

--- CodigoPython.py
+++ CodigoPython.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 08:55:42 2022
+
+@author: SALA
+"""
+
 import  random
 
 c=0
@@ -25,6 +32,22 @@ for i in range (1, valores+1):
         c=round(random.uniform(bplo+0.001,0.070),3)
         print("El valor redondeado para la concentración del contaminante es: ", c)
         
+        print("apartado particulas menores a 10 micrometros")
+        valorpp= round(random.uniform(1,40))
+        print("El valor de bphi es: ", valorpp)
+       
+        bplo=round(random.uniform(0,valorpp - 1))
+        print("El valor de bplo es: ", bplo)
+        k= ihi - ilo
+        resta= round((valorpp - bplo))
+        print("El valor de la resta es: ",resta)
+        k= round((k / resta),3)
+        print("El valor de K es: ",k)
+        c=round(random.uniform(bplo+1,40))
+        print("El valor redondeado para la concentración del contaminante es: ", c)
+        indice = k * (c - bplo)
+        indice= round((indice + ilo))
+        print("El indice de contaminación en Partículas Menores a 10 micrómetros (PM10) es: ", indice)
        
     if valorpp>=51 and valorpp<=110:
         bplo=round(random.uniform(0.071,valorpp - 0.001),3)
@@ -38,6 +61,23 @@ for i in range (1, valores+1):
         print("El valor de K es: ",k)
         c=round(random.uniform(bplo+0.001,0.095),3)
         print("El valor redondeado para la concentración del contaminante es: ", c)
+       
+        print("apartado particulas menores a 10 micrometros")
+        valorpp= round(random.uniform(41,75))
+        print("El valor de bphi es: ", valorpp)
+       
+        bplo=round(random.uniform(40,valorpp - 1))
+        print("El valor de bplo es: ", bplo)
+        k= ihi - ilo
+        resta= round((valorpp - bplo))
+        print("El valor de la resta es: ",resta)
+        k= round((k / resta),3)
+        print("El valor de K es: ",k)
+        c=round(random.uniform(bplo+1,75))
+        print("El valor redondeado para la concentración del contaminante es: ", c)
+        indice = k * (c - bplo)
+        indice= round((indice + ilo))
+        print("El indice de contaminación en Partículas Menores a 10 micrómetros (PM10) es: ", indice)
         
         
         print (f"la calidad del aire con un total de {valorpp} es regular ")
@@ -53,6 +93,24 @@ for i in range (1, valores+1):
         print("El valor de K es: ",k)
         c=round(random.uniform(bplo+0.001,0.154),3)
         print("El valor redondeado para la concentración del contaminante es: ", c)
+        print("\n")
+        print("\n")
+        print("apartado particulas menores a 10 micrometros")
+        valorpp= round(random.uniform(77,214))
+        print("El valor de bphi es: ", valorpp)
+        
+        bplo=round(random.uniform(76,valorpp - 1))
+        print("El valor de bplo es: ", bplo)
+        k= ihi - ilo
+        resta= round((valorpp - bplo))
+        print("El valor de la resta es: ",resta)
+        k= round((k / resta),3)
+        print("El valor de K es: ",k)
+        c=round(random.uniform(bplo+1,214))
+        print("El valor redondeado para la concentración del contaminante es: ", c)
+        indice = k * (c - bplo)
+        indice= round((indice + ilo))
+        print("El indice de contaminación en Partículas Menores a 10 micrómetros (PM10) es: ", indice)
         
         
         print (f"la calidad del aire con un total de {valorpp} es mala ")
@@ -68,6 +126,24 @@ for i in range (1, valores+1):
         print("El valor de K es: ",k)
         c=round(random.uniform(bplo+0.001,0.204),3)
         print("El valor redondeado para la concentración del contaminante es: ", c)
+        print("\n")
+        print("\n")
+        print("apartado particulas menores a 10 micrometros")
+        valorpp= round(random.uniform(216,354))
+        print("El valor de bphi es: ", valorpp)
+        
+        bplo=round(random.uniform(215,valorpp - 1))
+        print("El valor de bplo es: ", bplo)
+        k= ihi - ilo
+        resta= round((valorpp - bplo))
+        print("El valor de la resta es: ",resta)
+        k= round((k / resta),3)
+        print("El valor de K es: ",k)
+        c=round(random.uniform(bplo+1,354))
+        print("El valor redondeado para la concentración del contaminante es: ", c)
+        indice = k * (c - bplo)
+        indice= round((indice + ilo))
+        print("El indice de contaminación en Partículas Menores a 10 micrómetros (PM10) es: ", indice)
         
         
         print (f"la calidad del aire con un total de {valorpp} es muy mala ")
@@ -85,3 +161,21 @@ for i in range (1, valores+1):
         c=round(random.uniform(bplo+0.001,0.604),3)
         print("El valor redondeado para la concentración del contaminante es: ", c)
         print (f"la calidad del aire con un total de {valorpp} es extremadamente mala ")
+        print("\n")
+        print("\n")
+        print("apartado particulas menores a 10 micrometros")
+        valorpp= round(random.uniform(356,604))
+        print("El valor de bphi es: ", valorpp)
+        
+        bplo=round(random.uniform(355,valorpp - 1))
+        print("El valor de bplo es: ", bplo)
+        k= ihi - ilo
+        resta= round((valorpp - bplo))
+        print("El valor de la resta es: ",resta)
+        k= round((k / resta),3)
+        print("El valor de K es: ",k)
+        c=round(random.uniform(bplo+1,604))
+        print("El valor redondeado para la concentración del contaminante es: ", c)
+        indice = k * (c - bplo)
+        indice= round((indice + ilo))
+        print("El indice de contaminación en Partículas Menores a 10 micrómetros (PM10) es: ", indice)
